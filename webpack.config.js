d6566853481593620a5aaa25d03184b53c496968
Joseph Gefroh <joseph.gefroh@gmail.com>
2018-05-07 20:53:04 -0700

    Add uglification.

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,17 @@
 const path = require('path');
 const webpack = require('webpack');
-
 const CleanWebpackPlugin = require('clean-webpack-plugin'); //Used to clear the build directory between each build.
 const CopyWebpackPlugin = require('copy-webpack-plugin'); // Used to transfer static assets (html, images, etc.) as part of the build.
 const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin'); // Used to compile scss.
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // Used to generate an index.html file with proper hashed assets
 const PackageJson = require('./package.json');  //Used to automatically load dependencies into vendor.
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin"); //Used to obfuscate the JS for production
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin"); //Used to read CSS
+// const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin"); //Used to obfuscate the CSS for production
 
 
 const OUTPUT_FOLDER_NAME = 'build/'
-
+const IS_PRODUCTION_BUILD = (process.env.NODE_ENV == 'production');
 module.exports = {
     entry: {
       app: path.join(__dirname, '/app/app.js'),
@@ -72,6 +74,13 @@ module.exports = {
           to: 'assets'
         },
         ], {}
-      )
+      ),
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: !IS_PRODUCTION_BUILD, // set to true if you want JS source maps,
+        compress: IS_PRODUCTION_BUILD,
+        mangle: IS_PRODUCTION_BUILD
+      })
     ]
 };
